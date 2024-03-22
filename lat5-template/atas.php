17f09e485d9d995f10f5466fb02d46974e66aacb
hanafdh <162039710+hanafdh@users.noreply.github.com>
2024-03-22 21:16:04 +0700

    Add files via upload

---
+++ lat5-template/atas.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB-STTNF</title>
+</head>
+<body>
+    <header>
+        <h2>Mahasiswa STTNF Semester 3</h2>
+</header>
+
+<h1>Welcome Home</h1>
+
+<footer>
+    <p>Created by @sttnf</p>
+</footer>
+</body>
+</html>
+
+
