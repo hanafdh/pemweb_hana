db29d17380e5d464903c7ebda510eb2d0bac55d8
hanafdh <162039710+hanafdh@users.noreply.github.com>
2024-03-27 08:27:34 +0700

    tugas 4
    tugas 4

---
+++ class_persegipanjang.php
@@ -0,0 +1,11 @@
+<?php
+class PersegiPanjang{
+    var $panjang;
+    var $lebar;
+	
+    function __construct($panjang,$lebar){
+    	$this->panjang = $panjang;
+        $this->lebar = $lebar;
+        
+    }
+}
---
+++ rumus.php
@@ -0,0 +1,16 @@
+<?php
+require_once('week5\class_persegipanjang.php');
+
+function getLuas(){
+    $persegipanjang = new PersegiPanjang(4,5);
+    $luas = $persegipanjang->panjang * $persegipanjang->lebar;
+    echo "Luas = ".$luas . PHP_EOL;
+}
+function getKeliling(){
+    $persegipanjang = new PersegiPanjang(4,5);
+    $keliling = 2*($persegipanjang->panjang+$persegipanjang->lebar);
+    echo "Keliling = ". $keliling . PHP_EOL;
+}
+
+getLuas();
+getKeliling();
